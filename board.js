fe6c9d646af416c25f0c15de37e1e6189d2508b0
aaa0109 <aaawonder0109@gmail.com>
2024-05-15 16:55:24 -0300

    initial

--- board.js
+++ board.js
@@ -1,25 +1,13 @@
 import Tile from './tile.js';
 
 export default {
+  props: ['tiles'],
   template: `
     <div class="board">
-      <Tile marker="x" :index="0" @click="alert(0)" />
-      <Tile marker="o" :index="1" @click="alert(1)" />
-      <Tile marker="x" :index="2" @click="alert(2)" />
-      <Tile marker="o" :index="3" @click="alert(3)" />
-      <Tile marker="x" :index="4" @click="alert(4)" />
-      <Tile marker="o" :index="5" @click="alert(5)" />
-      <Tile marker="x" :index="6" @click="alert(6)" />
-      <Tile marker="o" :index="7" @click="alert(7)" />
-      <Tile marker="x" :index="8" @click="alert(8)" />
+      <Tile v-for="(tile, index) in tiles" :key="index" :marker="tile" :index="index" @click="$emit('click', index)" />
     </div>
   `,
   components: {
     Tile,
   },
-  methods: {
-    alert: (index) => {
-      window.alert(`Clicked on tile ${index}`);
-    },
-  }
 }
--- game.js
+++ game.js
@@ -6,16 +6,85 @@ import PlayAgain from './playAgain.js';
 export default {
   template: `
     <div class="game">
-      <Score :player1="5" :player2="3" />
-      <Board />
-      <Message :text="'It&rsquo;s a draw'" />
-      <PlayAgain />
+      <Score :player1="score.player1" :player2="score.player2" />
+      <Board :tiles="board" @click="makeMove" />
+      <Message :text="message" />
+      <PlayAgain v-if="gameOver" @click="resetGame" />
     </div>
-  `,
+    `,
   components: {
     Score,
     Board,
     Message,
     PlayAgain,
-  }
+  },
+  data() {
+    return {
+      board: Array(9).fill(null),
+      currentPlayer: Math.random() < 0.5 ? 'O' : 'X',
+      gameOver: false,
+      winner: null,
+      score: {
+        player1: 0,
+        player2: 0,
+      }
+    }
+  },
+  methods: {
+    makeMove(index) {
+      if (this.board[index] === null && !this.gameOver) {
+        this.board.splice(index, 1, this.currentPlayer);
+        this.checkEndConditions();
+        this.currentPlayer = this.currentPlayer === 'O' ? 'X' : 'O';
+      }
+    },
+    checkEndConditions() {
+      const winningCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ];
+      for (const combination of winningCombinations) {
+        const [a, b, c] = combination;
+        if (this.board[a] && this.board[a] === this.board[b] && this.board[a] === this.board[c]) {
+          this.gameOver = true;
+          this.winner = this.board[a];
+          if (this.winner === 'O') {
+            this.score.player1++;
+          } else if (this.winner === 'X') {
+            this.score.player2++;
+          }
+          return;
+        }
+      }
+      if (!this.board.includes(null)) {
+        this.gameOver = true;
+      }
+    },
+    resetGame() {
+      this.board.fill(null);
+      this.currentPlayer = Math.random() < 0.5 ? 'O' : 'X';
+      this.gameOver = false;
+      this.winner = null;
+    },
+  },
+  computed: {
+    score() {
+      return {
+        player1: this.winner === 'O' ? 1 : 0,
+        player2: this.winner === 'X' ? 1 : 0,
+      };
+    },
+    message() {
+      if (this.gameOver) {
+        return this.winner ? `Player ${this.winner === 'O' ? 1 : 2} wins` : "It's a draw";
+      }
+      return `It's player ${this.currentPlayer === 'O' ? 1 : 2}'s turn`;
+    },
+  },
 }
